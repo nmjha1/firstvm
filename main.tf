4c519732d4cc35cb08ecd7d61e3349fda268387c
nmjha1 <112245272+nmjha1@users.noreply.github.com>
2022-08-27 14:21:54 +0530

    first vm
    contains provider and ami id

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region="us-east-1"
+}
+
+resource "aws_instance" "myfirstserver" {
+    ami = "ami-05fa00d4c63e32376"
+    instance_type = "t2.micro"
+
+ tags = {
+    Name = "ExampleAppServerInstance"
+  }
+  
+}
